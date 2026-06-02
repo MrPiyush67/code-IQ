ab5d2c37c57314b3ba9deba544f5b204cad13e86
MrPiyush67 <mrpiyush67642005@gmail.com>
2026-06-02 19:30:31 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { ENV } from './lib/env.js';
+import path from 'path'
+
+const app = express();
+
+const __dirname = path.resolve();
+
+app.get('/health', (req, res) => {
+  return res.status(200).json({ msg: 'api is up and running' });
+});
+
+app.get('/books', (req, res) => {
+  return res.status(200).json({ msg: 'this is books endpoint' });
+});
+
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.json(__dirname, '../frontend/dist')));
+
+  app.get('./{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log('server is running on port ', ENV.PORT);
+});
