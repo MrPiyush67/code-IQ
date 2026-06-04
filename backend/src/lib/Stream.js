0518b4eaa537191657113842ede7d4df2a69fe61
MrPiyush67 <mrpiyush67642005@gmail.com>
2026-06-04 19:24:49 +0530

    storing user to stream using inngest

---
+++ backend/src/lib/Stream.js
@@ -0,0 +1,30 @@
+import { StreamChat } from 'stream-chat';
+import { ENV } from './env';
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error('steam apiKey or apiSecret is missing');
+}
+
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upserStreamUser = async(userData) => {
+    try {
+        await chatClient.upsertUser(userData)
+        console.log("stream user upserted successfully:",userData)
+    } catch (error) {
+        console.error("error upserting stream user:",error)
+        
+    }
+}
+export const deleteStreamUser = async(userId) => {
+    try {
+        await chatClient.deleteUser(userId)
+        console.log("stream user  deleted successfully:",userId)
+    } catch (error) {
+        console.error("error deleting the stream user:",error)
+        
+    }
+}
