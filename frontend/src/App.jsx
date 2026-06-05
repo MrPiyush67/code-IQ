d97e255ee41d7f8a5befee3b10fb79884cf55e3b
MrPiyush67 <mrpiyush67642005@gmail.com>
2026-06-05 19:55:34 +0530

    setting up tools and packages in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,21 @@
-import {
-  Show,
-  SignIn,
-  SignInButton,
-  SignOutButton,
-  SignUpButton,
-  UserButton,
-} from '@clerk/react';
+import { Navigate, Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage.jsx';
+import ProblemPage from './pages/ProblemPage.jsx';
+import { useUser } from '@clerk/react';
+import { Toaster } from 'react-hot-toast';
 
-/**
- * Root application component that displays a welcome header and conditional authentication controls.
- *
- * Renders a heading "welcome to the app". When the user is signed out, shows SignIn and SignUp buttons;
- * when signed in, shows the User menu button and SignOut button.
- *
- * @returns {JSX.Element} The app's top-level JSX containing the header and Clerk-based sign-in/sign-out UI.
- */
 function App() {
+  const { isSignedIn } = useUser();
   return (
     <>
-      <h1>welcome to the app</h1>
-
-      <Show when="signed-out">
-        <SignInButton />
-        <SignUpButton />
-      </Show>
-      <Show when="signed-in">
-        <UserButton />
-        <SignOutButton />
-      </Show>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/problem"
+          element={isSignedIn ? <ProblemPage /> : <Navigate to={'/'} />}
+        />
+      </Routes>
+      <Toaster />
     </>
   );
 }
